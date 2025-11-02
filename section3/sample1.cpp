8a030796180e4e9d2c3e290382d27a41aa4f7799
chibamo <dulcet71@gmail.com>
2025-11-02 03:30:54 +0000

    section3

---
+++ section3/sample1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "こんにちは\n";
+    cout << "1000\n";
+    cout << "1000円\n";
+    return 0;
+}
---
+++ section3/sample3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "こちらは" << 1000 << "円です\n";
+    return 0;
+}
