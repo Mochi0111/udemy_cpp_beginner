3b95240b867642d905485c5ed45b06dc27492be0
chibamo <dulcet71@gmail.com>
2025-11-08 15:43:54 +0900

    section9完了

---
+++ section9/sample2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void keisan(int *x){
+    *x = 25;
+}
+
+int main (void){
+    int apple = 10;
+    printf("1回目：%d\n", apple);
+    keisan(&apple);
+
+    printf("書き換えました\n");
+    printf("2回目：%d\n", apple);
+    return 0;
+}
